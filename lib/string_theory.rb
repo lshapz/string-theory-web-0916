f37bbe5f5ee0d9251f7d77748afb7f639e7e8098
lshapz <laurashapiro01@gmail.com>
2016-12-20 13:10:57 -0500

    Done.

--- lib/string_theory.rb
+++ lib/string_theory.rb
@@ -1,3 +1,24 @@
 def sort_by_appearance(characters_to_sort, long_text)
   # code goes here
+  char_array = characters_to_sort.split('')
+
+  new_hash = char_array.each_with_object({}) do |item, obj|
+    obj[item] = 0
+  end
+
+  text_array = long_text.split('')
+
+  text_array.each do |character|
+    if new_hash[character]
+      new_hash[character] += 1
+    end
+  end
+
+  last_array = new_hash.sort_by do |_key, value|
+    value
+  end
+
+  last_array.reverse.to_h.keys.join('')
+
+
 end
